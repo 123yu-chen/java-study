831cd0c8dc4e615c0d0aa293b3574fc29c67e735
123yu-chen <1934475345@qq.com>
2026-09-10 11:12:53 +0800

    chore: init repo (README, .gitignore, HelloWorld)

---
+++ src/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub!");
+        System.out.println("My first commit: 2026-09-10");
+    }
+}
